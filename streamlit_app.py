8a18d18902bb1649f02281632052e0b32df607a9
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 21:24:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,12 @@ st.set_page_config(
 st.markdown("""
   <div style="display: flex; justify-content: Center; align-items: Center;">
     <img src="https://cdn-icons-png.flaticon.com/128/2118/2118460.png" alt="RRHH YesBpo Logo" width="100" height="100">
-    <h1 style='color: #0f0a68; font-size: 29px;'> Proyecto Recursos Humanos</h1>
+    <h1 style='color: #0f0a68; font-size: 29px;'> ProductiApp</h1>
   </div>
 """, unsafe_allow_html=True)
     
 st.markdown("""
-    <h1 style='text-align: left; color: #0f0a68; font-size: 15px;'>Transparencia y claridad en cada paso. Conoce el estado de tus solicitudes y mantente informado sobre los procesos de RRHH. ¡Tu tranquilidad es nuestra prioridad!</h1>
+    <h1 style='text-align: left; color: #0f0a68; font-size: 15px;'> Bienvenido a ProductiApp, la herramienta definitiva para la supervisión y gestión de tareas. Simplifica tu día a día, mantén el control sobre tus proyectos y colabora eficientemente con tu equipo. Con ProductiApp, monitorear el progreso nunca fue tan fácil. ¡Empieza a transformar tu productividad hoy!</h1>
     """, unsafe_allow_html=True)
 
 #https://docs.google.com/spreadsheets/d/1RbnK8K17h7ttYIDlg9FHtIhkyK8RvrSBqWEeeoMXt68/edit?gid=0#gid=0
