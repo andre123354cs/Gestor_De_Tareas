baa7d9978a6a27f581c5e3fe167b8b9813f99858
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 21:37:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 
 st.set_page_config(
     page_title="MetaData",
@@ -26,12 +25,8 @@ gsheetid='1RbnK8K17h7ttYIDlg9FHtIhkyK8RvrSBqWEeeoMXt68'
 sheetod='0'
 url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
 
-df= pd.read_csv(url)
+df = pd.read_csv(url)
 
-
-    
-    
-    
 # Obtener los valores únicos de la columna 'estado de la tarea'
 estados_unicos = df['estado de la tarea'].unique()
 
@@ -40,7 +35,6 @@ funcionarios_unicos = df['funcionario'].unique()
 
 novedad_seleccionada = st.multiselect('Selecciona Estados', estados_unicos)
 funcionario_seleccionada = st.multiselect('Selecciona Funcionario', funcionarios_unicos)
-
 
 # Filtrar el DataFrame según las selecciones
 df_filtrado = df.copy()
@@ -54,10 +48,9 @@ if funcionario_seleccionada:
 # Mostrar el DataFrame filtrado
 st.dataframe(df_filtrado, use_container_width=True)
 
+# Crear gráfico de barras para visualizar las tareas por funcionario
+fig = px.bar(df_filtrado, x='funcionario', y='tarea', color='estado de la tarea', 
+             title='Tareas por Funcionario', labels={'tarea':'Cantidad de Tareas'}, 
+             barmode='group', height=400)
 
-
-
-
-
-
-    
+st.plotly_chart(fig, use_container_width=True)
